a7ad313dca8586cea6cf205127c584fda257afc6
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-09-24 23:50:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+//your JS code here. If requi
+const container=document.querySelector(".container");
+for(let i=0;i<800;i++){
+	let div=document.createElement("div")
+	div.className="square"
+	// div.style.backgroundColor = getRandomColor();
+	div.addEventListener("mouseover" , ()=>{
+		div.style.backgroundColor=getRandomColor();
+		setTimeout(()=>{
+			div.style.backgroundColor=""
+		},1000)
+	})
+	container.appendChild(div);
+}
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
